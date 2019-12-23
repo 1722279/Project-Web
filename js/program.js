daf3621c506da5f0167a1894a34defaedf429a2e
Y-T-G <yaseensinbox@gmail.com>
2019-12-23 23:24:51 +0800

    programs: Add slideshow

--- js/program.js
+++ js/program.js
@@ -1,3 +1,11 @@
+var pageindics = document.getElementsByClassName("indicsvg");
+var slides = document.getElementsByClassName("slide");
+var slideIndex = 0;
+var autoplay = setInterval(nextSlide, 5000);
+
+for(var i=0; i<pageindics.length; i++){
+    pageindics[i].addEventListener("click", jumpSlide.bind(this, i));
+}
 
 function scrollLef() {
     var cards = document.getElementsByClassName("card");
@@ -9,20 +17,20 @@ function scrollLef() {
             cards[i].classList.remove('lcard');
             cards[i].classList.add('mcard');
 
-            cards[(i+1)%length].classList.remove('mcard');
-            cards[(i+1)%length].classList.add('rcard');
+            cards[(i + 1) % length].classList.remove('mcard');
+            cards[(i + 1) % length].classList.add('rcard');
 
-            cards[(i+2)%length].classList.remove('rcard');
-            cards[(i+2)%length].classList.add('hidden');
-            
-            cards[(((i-1)%length)+length)%length].classList.add('lcard');
-            cards[(((i-1)%length)+length)%length].classList.remove('hidden');
-            
+            cards[(i + 2) % length].classList.remove('rcard');
+            cards[(i + 2) % length].classList.add('hidden');
+
+            cards[(((i - 1) % length) + length) % length].classList.add('lcard');
+            cards[(((i - 1) % length) + length) % length].classList.remove('hidden');
+
 
             break;
         }
     }
-    for(i=0; i < length-1; i++)
+    for (var i = 0; i < length - 1; i++)
         parent.appendChild(cards[0]);
 }
 
@@ -31,22 +39,60 @@ function scrollRight() {
     var el = cards[0];
     var parent = document.getElementsByClassName("card")[0].parentNode;
     var length = cards.length;
-    for (i = 0; i < cards.length; i++) {
+    for (var i = 0; i < cards.length; i++) {
         if (cards[i].classList.contains('lcard')) {
             cards[i].classList.remove('lcard');
             cards[i].classList.add('hidden');
 
-            cards[(i+1)%length].classList.remove('mcard');
-            cards[(i+1)%length].classList.add('lcard');
+            cards[(i + 1) % length].classList.remove('mcard');
+            cards[(i + 1) % length].classList.add('lcard');
 
-            cards[(i+2)%length].classList.remove('rcard');
-            cards[(i+2)%length].classList.add('mcard');
+            cards[(i + 2) % length].classList.remove('rcard');
+            cards[(i + 2) % length].classList.add('mcard');
 
-            cards[(i+3)%length].classList.add('rcard');
-            cards[(i+3)%length].classList.remove('hidden');
+            cards[(i + 3) % length].classList.add('rcard');
+            cards[(i + 3) % length].classList.remove('hidden');
 
             break;
         }
     }
     parent.appendChild(cards[0]);
+}
+
+function jumpSlide(times){
+    times = ((times-slideIndex)+slides.length)%slides.length;
+    for(var i=0; i<times; i++){
+        changeSlide(1);
+        console.log(i);
+    }
+    clearInterval(autoplay);
+    autoplay = setInterval(nextSlide, 5000);
+}
+
+function nextSlide(){
+    changeSlide(1);
+}
+
+function changeSlide(direction) {
+    var length = slides.length;
+
+    for (var i = 0; i < length; i++) {
+        if (!slides[i].classList.contains("hidden"))
+            if (direction == 1) {
+                slides[(i + 1)%length].classList.remove("hidden");
+                slides[i].classList.add("hidden");
+                pageindics[i].style.fill = 'rgba(0, 0, 0, 0)';
+                pageindics[(i+1)%length].style.fill = 'rgb(203, 203, 203)';
+                slideIndex = (slideIndex+1)%length;
+                break;
+            }
+            else if (direction == -1) {
+                slides[((i - 1)%length)+length].classList.remove("hidden");
+                slides[i].classList.add("hidden");
+                pageindics[i].style.fill = 'rgba(0, 0, 0, 0)';
+                pageindics[(i+1)%length].style.fill = 'rgb(203, 203, 203)';
+                slideIndex = ((slideIndex-1)%length)+length;
+                break;
+            }
+    }
 }
